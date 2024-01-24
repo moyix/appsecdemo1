873a6b90c30394dbfbdae5b78f30196dc703874b
Brendan Dolan-Gavitt <brendandg@nyu.edu>
2024-01-23 19:01:29 -0500

    add revolutionary new greeting feature

--- hello.c
+++ hello.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Gimme a name\n");
+    if (argc < 3) {
+        printf("Gimme a name and a number\n");
         return 1;
     }
-    // print the hllo message
-    printf("Hello, %s\n", argv[1]);
+    int i = atoi(argv[2]);
+    for (int j = 0; j < i; j++) {
+        // print the hello message
+        printf("Hello, %s\n", argv[1]);
+    }
     return 0;
 }
